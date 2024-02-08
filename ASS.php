ca6d16f0660784e019340d7a2442bfbc935ef84e
junia-dotcom <79664391+junia-dotcom@users.noreply.github.com>
2024-02-08 13:00:59 +0100

    Add files via upload

---
+++ ASS.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $int = 5;
+        $i = 0;
+        $str = "moyo";
+
+        $double = 1.3;
+        $d = 0.0;
+
+        echo $int + $i . "<br>";
+        echo $double + $d ."<br>";
+        echo "Your name is ". $str;
+
+
+
+
+
+    ?>
+</body>
+</html>
